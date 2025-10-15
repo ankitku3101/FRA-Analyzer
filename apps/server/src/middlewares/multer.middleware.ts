cdc13b632dac21742d6ed991d49c2a2d9e3533d1
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-10-15 23:09:29 +0530

    style: fix formats

--- apps/server/src/middlewares/multer.middleware.ts
+++ apps/server/src/middlewares/multer.middleware.ts
@@ -1,19 +1,30 @@
-import path from "path";
+import path from 'path';
 import { Request } from 'express';
 import multer, { FileFilterCallback } from 'multer';
 
 const ALLOWED_FORMATS = ['csv', 'txt', 'xlsx', 'xml'];
 
 const storage = multer.diskStorage({
-  destination: (_req: Request, _file: Express.Multer.File, cb: (error: Error | null, destination: string) => void) => {
+  destination: (
+    _req: Request,
+    _file: Express.Multer.File,
+    cb: (error: Error | null, destination: string) => void
+  ) => {
     // Files will be saved in the 'uploads/' directory
     cb(null, 'public/');
   },
-  filename: (_req: Request, file: Express.Multer.File, cb: (error: Error | null, filename: string) => void) => {
+  filename: (
+    _req: Request,
+    file: Express.Multer.File,
+    cb: (error: Error | null, filename: string) => void
+  ) => {
     // Create a unique filename to avoid overwrites: originalname-timestamp.extension
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    cb(null, `${path.parse(file.originalname).name}-${uniqueSuffix}${path.extname(file.originalname)}`);
-  }
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(
+      null,
+      `${path.parse(file.originalname).name}-${uniqueSuffix}${path.extname(file.originalname)}`
+    );
+  },
 });
 
 const fileFilter = (_req: Request, file: Express.Multer.File, cb: FileFilterCallback) => {
@@ -33,8 +44,8 @@ const upload = multer({
   fileFilter: fileFilter,
   limits: {
     // Set a file size limit (e.g., 10MB) to prevent large uploads
-    fileSize: 10 * 1024 * 1024
-  }
+    fileSize: 10 * 1024 * 1024,
+  },
 });
 
-export default upload
+export default upload;
--- apps/server/src/routes/parser.routes.ts
+++ apps/server/src/routes/parser.routes.ts
@@ -1,8 +1,8 @@
-import { Router } from "express";
-import upload from "../middlewares/multer.middleware";
+import { Router } from 'express';
+import upload from '../middlewares/multer.middleware';
 
 const router = Router();
 
-router.post('/upload',upload.fields([{name:"content",maxCount:5}]),)
+router.post('/upload', upload.fields([{ name: 'content', maxCount: 5 }]));
 
 export default router;
